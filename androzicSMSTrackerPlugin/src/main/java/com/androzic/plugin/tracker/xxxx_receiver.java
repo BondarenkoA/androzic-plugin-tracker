26413a82ed4b709a95e9028565feedf10d61e2ea
Alexander Bondarenko <sellin@bk.ru>
2017-11-29 00:51:47 +0300

    add xxxx_receiver class

---
+++ androzicSMSTrackerPlugin/src/main/java/com/androzic/plugin/tracker/xxxx_receiver.java
@@ -0,0 +1,17 @@
+package com.androzic.plugin.tracker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by bond on 28.11.2017.
+ */
+
+public class xxxx_receiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+
+    }
+}
